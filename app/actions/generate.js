3664565d2ff19781ec5d95cb27f178069e3594e9
islambenchaiba <mi.benchaiba@esi-sba.dz>
2024-12-22 11:10:22 +0100

    test

--- app/actions/generate.js
+++ app/actions/generate.js
@@ -6,6 +6,7 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 export async function generateQuote(prompt) {
   try {
+    console.log('API Key in production:', process.env.GOOGLE_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
     const result = await model.generateContent(`Generate 4 different inspirational quotes based on this context: ${prompt}. 
